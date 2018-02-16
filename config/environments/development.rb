2364d04588d223ee684f19ffec04ffb055f3df27
Chris Palmweaver <thepalmweaver@gmail.com>
2018-02-16 01:44:41 -0500

    added mailer for devise password recovery using cigarbarstogies@gmail.com

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,9 +28,22 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  
+
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+  address: 'smtp.gmail.com',
+  port: 587,
+  domain: 'gmail.com',
+  authentication: 'plain',
+  enable_starttls_auto: true,
+  user_name: 'cigarbarstogies@gmail.com',
+  password: 'fattizzi'
+  }
 
   config.action_mailer.perform_caching = false
 
