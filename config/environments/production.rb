2c3ccb43dd843b169911474a90a800939f746d38
Chris Palmweaver <thepalmweaver@gmail.com>
2018-02-19 23:16:17 -0500

    configured production mailer values for pass recovery

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,10 +9,30 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
+  config.action_mailer.default_url_options = { :host => 'app.shrewdpixel.com' }
+  Rails.application.routes.default_url_options[:host] = 'app.shrewdpixel.com'
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+  address: 'smtp.gmail.com',
+  port: 587,
+  domain: 'gmail.com',
+  authentication: 'plain',
+  enable_starttls_auto: true,
+  user_name: 'cigarbarstogies@gmail.com',
+  password: 'fattizzi'
+  }
+
+
 
   # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
   # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
