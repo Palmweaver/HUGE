d381d29f932a7fe02c388c9abca679201fc56141
Chris Palmweaver <thepalmweaver@gmail.com>
2018-02-19 23:24:27 -0500

    Severe, app breaking bug. Editing production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
   config.action_mailer.default_url_options = { :host => 'app.shrewdpixel.com' }
-  Rails.application.routes.default_url_options[:host] = 'app.shrewdpixel.com'
+
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
